587ac6d7ceee39cc9297bf7a4ccf7bf6242bd85b
Sergei Kazakov <thaellyra@gmail.com>
2025-02-24 23:45:54 +0300

    Изменена логика Main класса в соответствии с заданием "Курсовой проект. Задание по теме "Циклы""

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int filesCount = 0;
+
+        while (true) {
+            System.out.println("Введите путь к файлу и нажмите <Enter>:");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (isDirectory) {
+                System.out.println("Это путь к директории, повторите попытку!");
+                continue;
+            }
+
+            if (fileExists) {
+                filesCount++;
+                System.out.println("Путь указан верно" + "\nЭто файл номер " + filesCount + "\nПродолжим!");
+            } else {
+                System.out.println("Указан путь к несуществующему файлу, повторите попытку!");
+            }
+        }
     }
 }
